a1235b8435cf8ca6ccd88a034452c8602fdbf168
Mukunda Bera <138148933+Muku12288@users.noreply.github.com>
2025-07-11 00:39:52 +0530

    Time: 1110 ms (5.01%), Space: 55.4 MB (68.77%) - LeetHub

--- 0901-online-stock-span/0901-online-stock-span.java
+++ 0901-online-stock-span/0901-online-stock-span.java
@@ -1,23 +1,35 @@
 class StockSpanner {
-    Stack<Integer> stack = new Stack<>();
-    LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
-    int index=-1;
+    Stack<Integer> stack = new Stack<>();//
+    // LinkedHashMap<Integer, Integer> map = new LinkedHashMap<>();
+    List<Integer> arr;
+    int count;
+    // int index=-1;
     public StockSpanner() {
-        
+        arr = new ArrayList<>();
+        count =0;
     }
     
     public int next(int price) {
-        if(price.typeOf() == Object) return null;
+        // if(price.typeOf() == Object) return null;
 
-        map.put(index++, price);
-        int count =0;
-        for(int i=map.size(); i<0; i--){
-            
-            if(map.get(i)<= price){
+        // map.put(index++, price);
+        arr.add(price);
+        count=0;
+        for(int i=arr.size()-1; i>=0; i-- ){
+            if(arr.get(i) <= price){
                 count++;
+            }else{
+                break;
             }
-            break;
         }
+        
+        // for(int i=map.size(); i<0; i--){
+            
+        //     if(map.get(i)<= price){
+        //         count++;
+        //     }
+        //     break;
+        // }
 
         
         // stack.push(price);
