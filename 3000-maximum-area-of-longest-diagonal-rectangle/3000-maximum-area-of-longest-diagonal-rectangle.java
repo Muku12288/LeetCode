e6a34978b478118ed8f1386793dceb0fdc1bc0ce
Mukunda Bera <138148933+Muku12288@users.noreply.github.com>
2025-08-26 21:08:02 +0530

    Time: 1 ms (96%), Space: 44.2 MB (96.92%) - LeetHub

--- 3000-maximum-area-of-longest-diagonal-rectangle/3000-maximum-area-of-longest-diagonal-rectangle.java
+++ 3000-maximum-area-of-longest-diagonal-rectangle/3000-maximum-area-of-longest-diagonal-rectangle.java
@@ -1,6 +1,7 @@
 class Solution {
     
     public int areaOfMaxDiagonal(int[][] dimensions) {
+        /*
         int msum =0, sum=0,area=0;
         for(int i=0; i<dimensions.length; i++){
 
@@ -12,6 +13,20 @@ class Solution {
             }else if(sum == msum && area < dimensions[i][0] * dimensions[i][1]){
                 area = dimensions[i][0] * dimensions[i][1];
                 msum = sum;
+            }
+        }
+        return area;*/
+        int area=0;
+        double sum =0.0, maxsum =0.0;
+        for(int i=0; i<dimensions.length; i++){
+            //
+            sum = Math.sqrt((dimensions[i][0]*dimensions[i][0]) + (dimensions[i][1]*dimensions[i][1]));
+            if(sum > maxsum){
+                area = dimensions[i][0]*dimensions[i][1];
+                maxsum = sum;
+            }
+            else if(sum == maxsum && (dimensions[i][0]*dimensions[i][1]) > area){
+                area = dimensions[i][0]*dimensions[i][1];
             }
         }
         return area;
