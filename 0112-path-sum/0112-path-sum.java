06b12e4eef73499dd5005099adc8fbc3fc870b9f
Mukunda Bera <138148933+Muku12288@users.noreply.github.com>
2025-10-21 23:29:01 +0530

    Time: 0 ms (100%), Space: 43.2 MB (63.39%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -14,24 +14,15 @@
  * }
  */
 class Solution {
-    int sum =0;
-    boolean res= false;
 
     public boolean hasPathSum(TreeNode node, int targetSum) {
         if(node == null)return false;
 
-        
-        // sum+= node.val;
         if(node.left == null && node.right == null && node.val == targetSum) return true;
-        // if(node.left == null && node.right == null)sum-=node.val;
-
-        // boolean lv = ;
-        // boolean rv = ;
-
-        // if(!lv || !rv)sum-=node.val;
 
         return hasPathSum(node.left, targetSum-node.val) || hasPathSum(node.right, targetSum-node.val);
     }
 
+     
    
 }
