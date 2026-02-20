5f90e395956995c90c52f286322261388c962a29
Mukunda Bera <138148933+Muku12288@users.noreply.github.com>
2026-02-20 20:01:49 +0530

    Time: 5 ms (20.05%), Space: 46.4 MB (5.62%) - LeetHub

---
+++ 3010-divide-an-array-into-subarrays-with-minimum-cost-i/3010-divide-an-array-into-subarrays-with-minimum-cost-i.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int minimumCost(int[] nums) {
+
+        int sum=0;
+        ArrayList<Integer> arr = new ArrayList<>();
+
+        if(nums.length == 3){
+            for(int i: nums){
+                sum+=i;
+            }
+        }
+        else{
+            sum += nums[0];
+            for(int i=1; i<nums.length; i++){
+                arr.add(nums[i]);
+            }
+            Collections.sort(arr);
+            sum+= (arr.get(0)+arr.get(1));
+        }
+        
+
+    return sum;
+    }
+}
