00290cf294353cedee2081c671baf1c268cfc998
Mukunda Bera <138148933+Muku12288@users.noreply.github.com>
2026-04-12 12:03:33 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.java
+++ 3740-minimum-distance-between-three-equal-elements-i/3740-minimum-distance-between-three-equal-elements-i.java
@@ -1,15 +1,8 @@
 class Solution {
     public int minimumDistance(int[] nums) {
-        // Map<Integer, Integer> map = new HashMap<>();
-        // Set<Integer> set = new Set<>();
         if(nums.length<3){
             return -1;
         }
-
-        // for(int i=0; i<nums.length; i++){
-        //     set.add(nums[i]);
-        //     map.put(i, nums[i]);
-        // }
         int j=0, sum = Integer.MAX_VALUE;
         for(int i=0; i<nums.length; i++){
             j=0;
@@ -17,7 +10,6 @@ class Solution {
                 if(nums[k]== nums[i] && j==0){
                     j=k;
                 }else if(nums[k]== nums[i] && j!=0){
-                    System.out.println(i+" "+j+" "+k);
                     sum = Math.min(sum, Math.abs(i-j)+Math.abs(j-k)+Math.abs(k-i));
                 }
             }
