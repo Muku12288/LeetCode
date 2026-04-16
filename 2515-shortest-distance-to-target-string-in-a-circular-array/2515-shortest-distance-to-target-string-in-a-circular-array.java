ff851fd909fe35aea1f7c095b6267b83d390a749
Mukunda Bera <138148933+Muku12288@users.noreply.github.com>
2026-04-16 08:43:38 +0530

    Time: 1 ms (29.38%), Space: 46.8 MB (61.88%) - LeetHub

--- 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
+++ 2515-shortest-distance-to-target-string-in-a-circular-array/2515-shortest-distance-to-target-string-in-a-circular-array.java
@@ -4,18 +4,14 @@ class Solution {
 
         int i=startIndex,j=startIndex;
         if(words[i].equals(target))return 0;
-        
+
         while(count < n){
-            // i=(i+1)%n;
-            
             if(words[(i+1)%n].equals(target)){
-                System.out.println("Executed 1");
                 mini = Math.min(mini, count+1);
             }
             if(words[(j-1+n)%n].equals(target)){
-                System.out.println("Executed");
                 mini = Math.min(mini, count+1);
-                // Math.abs(n-(startIndex-((j-1+n)%n)))
+               
             }
             i++;
             j--;
