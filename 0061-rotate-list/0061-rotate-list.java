aabe6a9a3251c5d0df055f4a258714ff61174b0c
Mukunda Bera <138148933+Muku12288@users.noreply.github.com>
2026-05-05 22:23:36 +0530

    Time: 7 ms (1.33%), Space: 44.8 MB (9.63%) - LeetHub

---
+++ 0061-rotate-list/0061-rotate-list.java
@@ -0,0 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode rotateRight(ListNode head, int k) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        ListNode end=null;
+        ListNode node=head;
+        
+        while(node != null){
+            arr.add(node.val);
+            if(node.next == null){
+                //System.out.println(node.val);
+                end=node;
+                System.out.println(end.val +" "+ end.next);
+            }
+            node = node.next;
+        }
+        int len=arr.size();
+        //System.out.println(len);
+
+        if(len<=1){
+            return head;
+        }
+        else if(len<k || len>k){
+            if(len<k)k=k%len;
+            int n=Math.abs(len-k);
+            //System.out.println(n);
+            end.next = head;
+            //System.out.println(end.val +" "+ end.next.val);
+            ListNode itr =head;
+            for(int i=0; i<n; i++){
+                
+                if(i==n-1){
+                    head =itr.next;
+                    itr.next =null;
+                }
+                itr = itr.next;
+            }
+
+
+        }else if(len == k){
+            return head;
+        }
+    return head;
+    }
+}
