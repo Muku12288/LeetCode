c46210a74cf79201c0d2343ac08322061ae1f180
Mukunda Bera <138148933+Muku12288@users.noreply.github.com>
2026-05-08 23:31:49 +0530

    Time: 2196 ms (11.85%), Space: 51.7 MB (19.5%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -6,22 +6,12 @@ class Solution {
         Arrays.sort(g);
 
         for(int i=0; i<g.length; i++){
-            // boolean c =false;
-            // int num=0,p=0;
             for(int j=0;j<s.length;j++){
-                // if(c){
-                //     if(s[j]<num && s[j]>= g[i]){
-                //         p=j;
-                //     }if(j==s.length-1)s[p]=0;
-                // }
                 if(s[j]!= 0 && s[j]>= g[i]){
                     count++;
-                    // num = s[j];
                     s[j] =0;
                     break;
-                    // c=true;
-                    // p =j;
-                    // if(j==s.length-1)s[p]=0;
+                    
                 }
             }
         }
