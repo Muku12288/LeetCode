1e8536c7d1fdd930e524605bffb0a66b1db4bdf4
Mukunda Bera <138148933+Muku12288@users.noreply.github.com>
2026-05-09 00:48:43 +0530

    Time: 2 ms (99.6%), Space: 72.5 MB (29.18%) - LeetHub

--- 0860-lemonade-change/0860-lemonade-change.java
+++ 0860-lemonade-change/0860-lemonade-change.java
@@ -1,11 +1,10 @@
 class Solution {
     public boolean lemonadeChange(int[] bills) {
         int fc=0, tc=0;
-        // Arrays.sort(bills);
 
         for(int i=0;i<bills.length; i++){
             if(bills[i]==5)fc++;
-            if(bills[i]==10){
+            else if(bills[i]==10){
                 if(fc>=1){
                     fc--;
                     tc++;
@@ -13,17 +12,15 @@ class Solution {
                     return false;
                 }  
             }
-            if(bills[i]==20){
+            else{
                 if(fc<=0)return false;
                 else if(tc<=0 && fc<3)return false;
                 else if(tc>=1 && fc>=1){
                     tc--;
                     fc--;
-                    // 2c++;
                 }
                 else if(tc<=0 && fc>=3){
                     fc -=3;
-                    // 2c++;
                 }
             }
         }
