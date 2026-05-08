b957d26af13ecdc13debf7eb45c3b633c8356951
Mukunda Bera <138148933+Muku12288@users.noreply.github.com>
2026-05-09 00:00:13 +0530

    Time: 314 ms (11.85%), Space: 51.9 MB (6.68%) - LeetHub

--- 0455-assign-cookies/0455-assign-cookies.java
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -1,10 +1,11 @@
 class Solution {
     public int findContentChildren(int[] g, int[] s) {
-        int count=0;
+
+        int count=0, j=0,i=0;
         if(s.length == 0)return 0;
         Arrays.sort(s);
         Arrays.sort(g);
-
+/*      .........................O(N2) .........................
         for(int i=0; i<g.length; i++){
             for(int j=0;j<s.length;j++){
                 if(s[j]!= 0 && s[j]>= g[i]){
@@ -14,6 +15,17 @@ class Solution {
                     
                 }
             }
+        }*/
+        while(i<g.length && j<s.length){
+            System.out.println(g[i]+" "+s[j]);
+            if( s[j]>= g[i]){
+                j++;
+                i++;
+                count++;
+            }else if(s[j]<g[i]){
+                j++;
+            }
+            // i++;
         }
     return count;
     }
